e8a9dad2da491ded3449641e38487269b1f64d31
Ahmed-Seleem01 <ahmedseleem.c@gmail.com>
2023-01-26 19:40:52 +0200

    Add object to hold the main game components

---
+++ js/app.js
@@ -0,0 +1,12 @@
+const gameComponents = (function () {
+  const board = document.querySelector(".board");
+  const result = document.querySelector("#result");
+  let moves = 0;
+  const movesIncrement = function () {
+    this.moves += 1;
+  };
+  const getMoves = function (){
+    return this.moves;
+  };
+  return {board, moves, result, getMoves, movesIncrement};
+})();
